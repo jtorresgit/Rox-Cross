aa2d8c7b06e77779036588a72b6ab29875e85acc
jtorresgit <52751270+jtorresgit@users.noreply.github.com>
2019-07-12 14:55:49 -0500

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -20,10 +20,7 @@ $this->title = 'My Yii Application';
             <div class="col-lg-12">
                 <h2>Description</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p></p>
                 <table class="table">
                     <thead>
                         <tr>
